e19ca8032c77d513b1678e728fd4412f2724491a
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-08-21 23:47:21 +0300

    commit tree 0a5ab1d8c84de7b66fd12507b34a17fe6aa18ed5

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,22 @@
-# Build a class EmailAddressParser that accepts a string of unformatted 
+# Build a class EmailAddressParser that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailAddressParser
+  def initialize(email)
+    @email = email
+    @email
+  end
+
+  def parse
+   pp @email.split(" ").grep(/[\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z]/i)
+    parsed_emails = @email.split(" ").map do |email|
+       email.split('').filter { |letter|
+        letter != ","
+       }.join
+    end
+    
+    parsed_emails.uniq
+  end
+end
